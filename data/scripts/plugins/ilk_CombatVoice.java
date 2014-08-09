c52eae9770a79b6cbeeba020c3c286259eb285c1
Jeff Stafford <jeff.stafford@live.com>
2014-08-09 02:20:43 -0700

    more voice stuff

--- data/scripts/plugins/ilk_CombatVoice.java
+++ data/scripts/plugins/ilk_CombatVoice.java
@@ -4,16 +4,26 @@ import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.combat.CombatEngineAPI;
 import com.fs.starfarer.api.combat.EveryFrameCombatPlugin;
 import com.fs.starfarer.api.combat.ShipAPI;
+import com.fs.starfarer.api.combat.ShipEngineControllerAPI;
 import com.fs.starfarer.api.util.IntervalUtil;
 import java.util.List;
+import java.util.Iterator;
 
 public class ilk_CombatVoice implements EveryFrameCombatPlugin {
-
+    
+    //define combat voice sounds and booleans for whether it's already warned you or not
     private static final String DAMAGE_WARN = "damage_warning";
-    private static boolean hull_warn = false;
+    private static boolean damage_warn_bool = false;
+
+    private static final String DAMAGE_CRITICAL = "damage_critical";
+    private static boolean damage_critical_bool = false;
+    
+    private static final String ENGINES_OFFLINE = "engines_offline";
+    private static boolean engine_failure_bool = false;
+
     private CombatEngineAPI engine;
 
-    private final IntervalUtil VoiceFrameTracker = new IntervalUtil(1f, 1f);
+    private final IntervalUtil VoiceFrameTracker = new IntervalUtil(0.5f, 1f);
 
     @Override
     public void advance(float amount, List events) {
@@ -26,11 +36,41 @@ public class ilk_CombatVoice implements EveryFrameCombatPlugin {
 
         VoiceFrameTracker.advance(amount);
         player = engine.getPlayerShip();
+
         if (VoiceFrameTracker.intervalElapsed()) {
-            //play DAMAGE_WARN sound if player health drops to 10%
-            if ((player.getHitpoints() / player.getMaxHitpoints() < 0.1) && (hull_warn == false)) {
+
+            //play DAMAGE_CRITICAL sound if player health drops to 10% and it hasn't already told you
+            if ((player.getHitpoints() / player.getMaxHitpoints() < 0.1) && (damage_critical_bool == false)) {
+                Global.getSoundPlayer().playSound(DAMAGE_CRITICAL, 1f, 1f, player.getLocation(), player.getVelocity());
+                damage_warn_bool = true;
+                damage_critical_bool = true;
+            }
+
+            //play DAMAGE_WARN sound if player health drops to 50%
+            if ( (player.getHitpoints() / player.getMaxHitpoints() < 0.4) && (damage_warn_bool == false)) {
                 Global.getSoundPlayer().playSound(DAMAGE_WARN, 1f, 1f, player.getLocation(), player.getVelocity());
-                hull_warn = true;
+                damage_warn_bool = true;
+            }
+
+            //play ENGINES_DISABLED sound if all player engines are disabled
+            java.util.List<ShipEngineControllerAPI.ShipEngineAPI> engines_list = player.getEngineController().getShipEngines();
+            ShipEngineControllerAPI.ShipEngineAPI an_engine;
+            
+            //iterate through player engines
+            for (Iterator iter = engines_list.iterator(); iter.hasNext();) {
+                an_engine = (ShipEngineControllerAPI.ShipEngineAPI) iter.next();
+                //break if an engine is active
+                if (!an_engine.isDisabled()) {
+                    engine_failure_bool = false;
+                    break;
+                //break if its told you about your engines being disabled already
+                } else if (engine_failure_bool) {
+                    break;
+                //ok, so everything's disabled... now play the sound...
+                } else {
+                    Global.getSoundPlayer().playSound(ENGINES_OFFLINE, 1f, 1f, player.getLocation(), player.getVelocity());
+                    engine_failure_bool = true;
+                }
             }
         }
     }
