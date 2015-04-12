b6022e1e0202d5e1ce0d2d5d7d688212627dbbc0
Jeff Stafford <jeff.stafford@live.com>
2015-04-11 21:48:20 -0700

    bubble shield

---
+++ jars/src/data/shipsystems/scripts/BubbleShieldStats.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package data.shipsystems.scripts;
+
+import com.fs.starfarer.api.combat.MutableShipStatsAPI;
+import com.fs.starfarer.api.combat.ShieldAPI;
+import com.fs.starfarer.api.combat.ShipAPI;
+import com.fs.starfarer.api.plugins.ShipSystemStatsScript;
+
+/**
+ *
+ * @author Jeff
+ */
+public class BubbleShieldStats implements ShipSystemStatsScript {
+
+    private boolean start = false;
+    private float orig;
+    
+    @Override
+    public void apply(MutableShipStatsAPI stats, String id, State state, float effectLevel) {
+        if (!(stats.getEntity() instanceof ShipAPI)) {
+            return;
+        }
+        ShipAPI ship = (ShipAPI) stats.getEntity();
+        
+        ShieldAPI shield = ship.getShield();
+        
+        if (!start) {
+            orig = shield.getArc();
+            start = true;
+        }
+        
+        //Expand shield to 360, fade in shield
+        if (state == State.IN) {
+            shield.setArc(effectLevel * (360 - orig) + orig);
+            stats.getShieldDamageTakenMult().modifyMult(id, 1-effectLevel);
+            stats.getShieldUnfoldRateMult().modifyMult(id, 2f);
+        }
+        
+        if (state == State.ACTIVE) {
+            shield.setArc(360f);
+            stats.getShieldDamageTakenMult().modifyMult(id, 0f);
+        }
+        
+        //Close shield
+        if (state == State.OUT) {
+            shield.setArc(effectLevel * (360 - orig) + orig);
+            stats.getShieldDamageTakenMult().modifyMult(id, 1-effectLevel);
+        }
+        
+        if ((effectLevel == 0) && (start)) {
+            start = false;
+        }
+    }
+
+    @Override
+    public void unapply(MutableShipStatsAPI stats, String id) {
+        stats.getShieldDamageTakenMult().unmodify(id);
+        stats.getShieldUnfoldRateMult().unmodify(id);
+    }
+
+    @Override
+    public StatusData getStatusData(int index, State state, float effectLevel) {
+        if (index == 0) {
+            return new StatusData("invulnerable", false);
+        }
+        return null;
+    }
+    
+}
