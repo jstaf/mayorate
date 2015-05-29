dd39b5f9d2c7a830e5c30eddbcc3c54a26110fab
Jeff Stafford <jeff.stafford@live.com>
2015-05-28 19:33:57 -0700

    IT WORKS OMFG

--- jars/src/data/scripts/plugins/ilk_DisruptorPlugin.java
+++ jars/src/data/scripts/plugins/ilk_DisruptorPlugin.java
@@ -1,31 +1,25 @@
 package data.scripts.plugins;
 
-import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.combat.BaseEveryFrameCombatPlugin;
 import com.fs.starfarer.api.combat.CombatEngineAPI;
+import com.fs.starfarer.api.combat.CombatEntityAPI;
 import com.fs.starfarer.api.combat.DamageType;
 import com.fs.starfarer.api.combat.DamagingProjectileAPI;
-import com.fs.starfarer.api.combat.ShipAPI;
 import com.fs.starfarer.api.combat.ViewportAPI;
 import com.fs.starfarer.api.input.InputEventAPI;
 import java.awt.Color;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import org.apache.log4j.Level;
-import org.lazywizard.lazylib.combat.entities.SimpleEntity;
+import org.lazywizard.lazylib.MathUtils;
+import org.lazywizard.lazylib.combat.entities.AnchoredEntity;
 import org.lwjgl.util.vector.Vector2f;
 
 public class ilk_DisruptorPlugin extends BaseEveryFrameCombatPlugin {
 
     private static final Color EMP_CORE_COLOR = new Color(255, 255, 255, 255);
-    private static final Color EMP_FRINGE_COLOR = new Color(232, 14, 86, 200);
+    private static final Color EMP_FRINGE_COLOR = new Color(232, 14, 86, 150);
 
     private float interval = 0f;
     private static final float THRESHOLD = 0.1f;
-
-    private HashMap<DamagingProjectileAPI, Vector2f> projMap = new HashMap<>();
 
     CombatEngineAPI engine;
 
@@ -52,32 +46,24 @@ public class ilk_DisruptorPlugin extends BaseEveryFrameCombatPlugin {
                     return;
                 }
 
-                if (spec.contains("ilk_disruptor_shot") && !projMap.containsKey(proj)) {
-                    projMap.put(proj, proj.getLocation());
-                }
-            }
-
-            //now iterate through the map and make emparcs
-            Iterator iter = projMap.entrySet().iterator();
-            while (iter.hasNext()) {
-                Map.Entry pair = (Map.Entry) iter.next();
-                DamagingProjectileAPI projectile = (DamagingProjectileAPI) pair.getKey();
-
-                //remove projectiles that no longer are in the engine
-                if (projectile == null) {
-                    iter.remove();
-                } else {
-                    //update projectiles
-                    Vector2f loc = (Vector2f) pair.getValue();
-                    //Global.getLogger(ilk_DisruptorPlugin.class).log(Level.DEBUG, "Made it! loc:" + loc.x + ", " + loc.y);
-                    engine.spawnEmpArc(projectile.getSource(), projectile.getLocation(), null, new SimpleEntity(loc), //owner, location, anchor, target
-                            DamageType.ENERGY, 0f, 0f, //damage stats
-                            1000000f, null, 12f, EMP_FRINGE_COLOR, EMP_CORE_COLOR); //maxrange, sfx, width, fringe, core
-
-                    pair.setValue(projectile.getLocation());
+                if (spec.contains("ilk_disruptor_shot")) {
+                    createFX(proj);
                 }
             }
         }
+    }
+
+    private void createFX(DamagingProjectileAPI projectile) {
+        //Vector2f randomPoint = MathUtils.getRandomPointOnCircumference(projectile.getLocation(), 50f);
+        Vector2f loc = projectile.getLocation();
+        Vector2f vel = (Vector2f) new Vector2f(projectile.getVelocity()).normalise().scale(60f * (float) Math.random());
+        Vector2f point = Vector2f.sub(loc, vel, new Vector2f());
+        CombatEntityAPI anchor1 = new AnchoredEntity(projectile, MathUtils.getRandomPointOnCircumference(point, 5));
+        engine.spawnEmpArc(projectile.getSource(), loc, //source,  startLocation
+                anchor1, //anchor
+                anchor1, //target
+                DamageType.ENERGY, 0f, 0f, //damage stats
+                1000f, null, 5f, EMP_FRINGE_COLOR, EMP_CORE_COLOR); //maxrange, sfx, width, fringe, core
     }
 
     @Override
