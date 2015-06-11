fb3ab67e61646f2657f0b0d903263fd9858d734d
Jeff Stafford <jeff.stafford@live.com>
2015-06-10 22:10:54 -0700

    removeMarketConditions fixed, both conditions commands take multiple inputs now

--- jars/src/com/fs/starfarer/api/impl/campaign/rulecmd/addMarketCondition.java
+++ jars/src/com/fs/starfarer/api/impl/campaign/rulecmd/addMarketCondition.java
@@ -3,7 +3,6 @@ package com.fs.starfarer.api.impl.campaign.rulecmd;
 import com.fs.starfarer.api.campaign.InteractionDialogAPI;
 import com.fs.starfarer.api.campaign.econ.MarketAPI;
 import com.fs.starfarer.api.campaign.rules.MemoryAPI;
-import com.fs.starfarer.api.impl.campaign.rulecmd.BaseCommandPlugin;
 import com.fs.starfarer.api.util.Misc;
 import java.util.List;
 import java.util.Map;
@@ -19,7 +18,9 @@ public class AddMarketCondition extends BaseCommandPlugin {
             return false;
         }
         
-        market.addCondition(params.get(0).getString(memoryMap));
+        for (Misc.Token param : params) {
+            market.addCondition(param.getString(memoryMap));
+        }
         
         return true;
     }
