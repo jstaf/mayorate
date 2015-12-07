3f2e4a87b333fbc06e1fdbe9f2f282ace5718aa8
Jeff Stafford <jeff.stafford@live.com>
2015-12-06 20:17:03 -0800

    music now resets properly after missions... forgot the key file

---
+++ src/data/scripts/plugins/MusicReset.java
@@ -0,0 +1,37 @@
+package data.scripts.plugins;
+
+import com.fs.starfarer.api.GameState;
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.Script;
+import com.fs.starfarer.api.combat.CombatEngineAPI;
+import com.fs.starfarer.api.combat.EveryFrameCombatPlugin;
+import com.fs.starfarer.api.combat.ViewportAPI;
+import com.fs.starfarer.api.input.InputEventAPI;
+import org.apache.log4j.Level;
+
+import java.util.List;
+
+public class MusicReset implements EveryFrameCombatPlugin {
+
+    @Override
+    public void advance(float amount, List<InputEventAPI> events) {
+        if (Global.getCurrentState().equals(GameState.TITLE) && !Global.getSoundPlayer().getCurrentMusicId().equals("miscallenous_main_menu.ogg")) {
+            Global.getSoundPlayer().playMusic(0, 0, "music_title");
+        }
+    }
+
+    @Override
+    public void renderInWorldCoords(ViewportAPI viewport) {
+
+    }
+
+    @Override
+    public void renderInUICoords(ViewportAPI viewport) {
+
+    }
+
+    @Override
+    public void init(CombatEngineAPI engine) {
+
+    }
+}
