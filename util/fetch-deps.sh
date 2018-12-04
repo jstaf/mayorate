a229f488fd368dc360107eae6e31c0167908e034
Jeff Stafford <jeff.stafford@live.com>
2018-12-03 21:15:50 -0500

    update dependency fetching

--- util/fetch-deps.sh
+++ util/fetch-deps.sh
@@ -5,13 +5,13 @@ cd ..
 
 if [ ! -d GraphicsLib ]; then
 	# graphicslib
-	wget https://bitbucket.org/DarkRevenant/graphicslib/downloads/GraphicsLib%201.2.0.7z
+    wget https://bitbucket.org/DarkRevenant/graphicslib/downloads/GraphicsLib%201.3.0.7z
 	7z x GraphicsLib*.7z
 fi
 
 if [ ! -d LazyLib ]; then
 	# lazylib
-	wget https://bitbucket.org/LazyWizard/lazylib/downloads/LazyLib%202.2.zip
+    wget https://bitbucket.org/LazyWizard/lazylib/downloads/LazyLib%202.4b.zip
 	unzip LazyLib*.zip
 fi
 
@@ -19,3 +19,4 @@ if [ ! -d Nexerelin ]; then
 	wget https://bitbucket.org/Histidine/exerelin/downloads/Nexerelin_0.8.3e.zip
 	unzip Nexerelin*.zip
 fi
+
