189623b6f8868ae451025fa3cae59654f85d986c
Jeff Stafford <jeff.stafford@protonmail.com>
2019-07-05 21:32:57 -0400

    sensor suite tweaks

--- src/main/data/hullmods/ilk_SensorSuite.java
+++ src/main/data/hullmods/ilk_SensorSuite.java
@@ -2,13 +2,14 @@ package data.hullmods;
 
 import com.fs.starfarer.api.combat.BaseHullMod;
 import com.fs.starfarer.api.combat.MutableShipStatsAPI;
+import com.fs.starfarer.api.combat.ShipAPI;
 import com.fs.starfarer.api.combat.ShipAPI.HullSize;
 
 public class ilk_SensorSuite extends BaseHullMod {
 
   public static final float SENSOR_RANGE_PERCENT = 50f;
   public static final float WEAPON_RANGE_PERCENT = 5f;
-  public static final float CAMPAIGN_SENSOR_BOOST = 2f;
+  public static final float CAMPAIGN_SENSOR_BOOST = 40f;
 
   @Override
   public void applyEffectsBeforeShipCreation(HullSize hullSize, MutableShipStatsAPI stats, String id) {
@@ -28,6 +29,17 @@ public class ilk_SensorSuite extends BaseHullMod {
     }
     if (index == 2) {
       return "" + (int) CAMPAIGN_SENSOR_BOOST;
+    }
+    return null;
+  }
+
+  public boolean isApplicableToShip(ShipAPI ship) {
+    return !ship.getVariant().hasHullMod("hiressensors");
+  }
+
+  public String getUnapplicableReason(ShipAPI ship) {
+    if (ship.getVariant().hasHullMod("hiressensors")) {
+      return "Incompatible with High-Resolution Sensors";
     }
     return null;
   }
