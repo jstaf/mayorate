88881a953d3dc98d93a83c62e907b3d2df609ec6
Jeff Stafford <jeff.stafford@protonmail.com>
2019-08-03 21:42:48 -0400

    misc playtesting changes

--- src/main/ilk/hullmods/AICrew.java
+++ src/main/ilk/hullmods/AICrew.java
@@ -6,17 +6,11 @@ import com.fs.starfarer.api.combat.ShipAPI;
 
 public class AICrew extends BaseHullMod {
 
-  public static final float PERCENT_AI_CREW = 0.5f;
   public static final float AI_EFFICIENCY = 0.25f;
-  public static final float AI_REPAIR_EFFICIENCY = 0.5f;
+  public static final float AI_REPAIR_EFFICIENCY = 0.2f;
 
   @Override
   public void applyEffectsBeforeShipCreation(ShipAPI.HullSize hullSize, MutableShipStatsAPI stats, String id) {
-    // crew mods
-    stats.getCrewLossMult().modifyMult(id, PERCENT_AI_CREW);
-    stats.getMinCrewMod().modifyMult(id, PERCENT_AI_CREW);
-    stats.getMaxCrewMod().modifyMult(id, PERCENT_AI_CREW);
-
     // weapon mods
     stats.getAutofireAimAccuracy().modifyFlat(id, AI_EFFICIENCY);
 
@@ -26,20 +20,18 @@ public class AICrew extends BaseHullMod {
     stats.getTurnAcceleration().modifyMult(id, 1f + AI_EFFICIENCY);
 
     // logistical penalties
-    stats.getCombatEngineRepairTimeMult().modifyMult(id, 1.0f - AI_REPAIR_EFFICIENCY * PERCENT_AI_CREW);
-    stats.getCombatWeaponRepairTimeMult().modifyMult(id, 1.0f - AI_REPAIR_EFFICIENCY * PERCENT_AI_CREW);
-    stats.getRepairRatePercentPerDay().modifyMult(id, 1.0f - AI_REPAIR_EFFICIENCY * PERCENT_AI_CREW);
+    stats.getCombatEngineRepairTimeMult().modifyMult(id, 1.0f - AI_REPAIR_EFFICIENCY);
+    stats.getCombatWeaponRepairTimeMult().modifyMult(id, 1.0f - AI_REPAIR_EFFICIENCY);
+    stats.getRepairRatePercentPerDay().modifyMult(id, 1.0f - AI_REPAIR_EFFICIENCY);
   }
 
   @Override
   public String getDescriptionParam(int index, ShipAPI.HullSize hullSize) {
     switch (index) {
     case 0:
-      return "" + (int) (PERCENT_AI_CREW * 100f);
-    case 1:
       return "" + (int) (AI_EFFICIENCY * 100f);
-    case 2:
-      return "" + (int) (AI_REPAIR_EFFICIENCY * PERCENT_AI_CREW * 100f);
+    case 1:
+      return "" + (int) (AI_REPAIR_EFFICIENCY * 100f);
     default:
       return null;
     }
