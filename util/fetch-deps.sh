2faf5a44aa7a4768b98acf461983d599ca03b9f4
Jeff Stafford <jeff.stafford@protonmail.com>
2019-08-15 20:06:05 -0400

    update deps

--- util/fetch-deps.sh
+++ util/fetch-deps.sh
@@ -5,13 +5,13 @@ cd ..
 
 if [ ! -d GraphicsLib ]; then
 	  # graphicslib
-    wget https://bitbucket.org/DarkRevenant/graphicslib/downloads/GraphicsLib%201.3.0.7z
+    wget https://bitbucket.org/DarkRevenant/graphicslib/downloads/GraphicsLib%201.4.0.7z
     7z x GraphicsLib*.7z
 fi
 
 if [ ! -d LazyLib ]; then
 	  # lazylib
-    wget https://bitbucket.org/LazyWizard/lazylib/downloads/LazyLib%202.4b.zip
+    wget https://bitbucket.org/LazyWizard/lazylib/downloads/LazyLib%202.4e.zip
 	  unzip LazyLib*.zip
 fi
 
