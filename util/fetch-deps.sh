642a78f3ea11e137b2006d75752afe69b14185fc
Jeff Stafford <jeff.stafford@protonmail.com>
2024-04-20 23:45:10 -0400

    update dependencies

--- util/fetch-deps.sh
+++ util/fetch-deps.sh
@@ -5,22 +5,22 @@ cd ..
 
 if [ ! -d GraphicsLib ]; then
 	# graphicslib
-    wget https://bitbucket.org/DarkRevenant/graphicslib/downloads/GraphicsLib_1.6.1.7z
+    wget https://bitbucket.org/DarkRevenant/graphicslib/downloads/GraphicsLib_1.9.0.7z
     7z x GraphicsLib*.7z
 fi
 
 if [ ! -d LazyLib ]; then
 	# lazylib
-    wget https://github.com/LazyWizard/lazylib/releases/download/2.7b/LazyLib.2.7b.zip
+    wget https://github.com/LazyWizard/lazylib/releases/download/2.8b/LazyLib.2.8b.zip
   	unzip LazyLib*.zip
 fi
 
 if [ ! -d Nexerelin ]; then
-    wget https://github.com/Histidine91/Nexerelin/releases/download/v0.10.6d/Nexerelin_0.10.6d.zip
+    wget https://github.com/Histidine91/Nexerelin/releases/download/v0.11.1b/Nexerelin_0.11.1b.zip
     unzip Nexerelin*.zip
 fi
 
 if [ ! -d MagicLib ]; then
-    wget https://github.com/MagicLibStarsector/MagicLib/releases/download/0.46.1-rc03/MagicLib-0.46.1-rc03.zip
+    wget https://github.com/MagicLibStarsector/MagicLib/releases/latest/download/MagicLib.zip
     unzip MagicLib*.zip
 fi
